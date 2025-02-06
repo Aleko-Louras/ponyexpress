1ebe9996bc19860d71f6e60626c60d61e643e1ad
Aleko-Louras <123419605+Aleko-Louras@users.noreply.github.com>
2025-02-05 22:45:39 -0700

    test fix

--- backend/routers/memberships.py
+++ backend/routers/memberships.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
+from starlette.responses import JSONResponse
+
 from backend.database import membership as MembershipRepository
 from backend.dependencies import DBSession
 from backend.models import MembershipCreate, Membership
@@ -7,13 +9,21 @@ from backend.exceptions import EntityNotFound, ChatMembershipRequired, ChatOwner
 router = APIRouter(prefix="/chats", tags=["Memberships"])
 
 
+# @router.post("/{chat_id}/accounts", response_model=Membership)
+# def add_membership(chat_id: int, membership_create: MembershipCreate, session: DBSession):
+#     try:
+#         return MembershipRepository.add_membership(session, chat_id, membership_create)
+#     except EntityNotFound as e:
+#         raise HTTPException(status_code=404, detail={"error": e.error, "message": e.message})
+
 @router.post("/{chat_id}/accounts", response_model=Membership)
 def add_membership(chat_id: int, membership_create: MembershipCreate, session: DBSession):
     try:
-        return MembershipRepository.add_membership(session, chat_id, membership_create)
+        new_membership = MembershipRepository.add_membership(session, chat_id, membership_create)
+        status_code = 201 if new_membership else 200  # ✅ Return 201 if new membership is created
+        return JSONResponse(status_code=status_code, content=new_membership.dict())
     except EntityNotFound as e:
         raise HTTPException(status_code=404, detail={"error": e.error, "message": e.message})
-
 
 @router.delete("/{chat_id}/accounts/{account_id}", status_code=204)
 def remove_membership(chat_id: int, account_id: int, session: DBSession):
