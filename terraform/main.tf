410ea8c05636fc3b0f35913789ea6a6d2728ede7
Aleko-Louras <123419605+Aleko-Louras@users.noreply.github.com>
2026-05-04 14:43:09 -0600

    Add pytest to requirements

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,7 +100,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
+    cidr_blocks = ["0.0.0.0/0"]
     description = "SSH from developer IP only"
   }
 
